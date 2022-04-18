561be3d8a2233ce7f2ec75d5ab353cecc05d3a9e
Martinupset <1023090715@qq.com>
2022-04-18 16:08:26 +0800

    day4

--- demo-gs-1/src/main/java/com/example/demogs1/controller/EmpController.java
+++ demo-gs-1/src/main/java/com/example/demogs1/controller/EmpController.java
@@ -1,13 +1,35 @@
 package com.example.demogs1.controller;
 
+import com.example.demogs1.model.Emp;
+import com.example.demogs1.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //use to created get method request api
 @RestController
 public class EmpController {
+  //DI => create an obj and Injected => default: singleton
+  @Autowired
+  EmpService empService;
+
   @GetMapping("/hello")
   public String hw() {
     return "hello world";
+  }
+
+  @GetMapping("/emps")
+  public List<Emp> getAll () {
+    return empService.getEmpList();
+  }
+
+  @PostMapping("/emp")
+  public Boolean add (@RequestBody Emp emp) {
+    System.out.println(emp);
+    return empService.add(emp);
   }
 }
